c15e4d51401d9cbfea4925d33239155ced3082d5
Arakhor <csmolenski23@gmail.com>
2023-08-22 08:31:25 +0200

    First version

---
+++ index.js
@@ -0,0 +1,68 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+	getDatabase,
+	ref,
+	push,
+	onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+	databaseURL:
+		"https://we-are-the-champions-24398-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const listInDB = ref(database, "forms");
+
+const formEl = document.getElementById("form");
+const msgEl = document.getElementById("msg");
+const senderEl = document.getElementById("sender");
+const recipientEl = document.getElementById("recipient");
+const formListEl = document.getElementById("form-list");
+
+//
+onValue(listInDB, (snapshot) => {
+	if (snapshot.exists()) {
+		let itemsArr = Object.entries(snapshot.val());
+		clearFormListEl();
+		for (let i = 0; i < itemsArr.length; i++) {
+			appendToFormListEl(itemsArr[i]);
+		}
+	}
+});
+
+// submit form
+formEl.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	if (msgEl.value == "" || senderEl.value == "" || recipientEl.value == "") {
+		console.log("error");
+	} else if (msgEl.value.length > msgEl.maxLength) {
+		console.log("error");
+	} else {
+		const item = {
+			msg: msgEl.value,
+			sender: senderEl.value,
+			recipient: recipientEl.value,
+		};
+		push(listInDB, item);
+		clearInput();
+	}
+});
+
+function appendToFormListEl(item) {
+	let newEl = document.createElement("li");
+	newEl.innerHTML = `<h5>To ${item[1].recipient}</h5><p>${item[1].msg}</p><h5>From ${item[1].sender}</h5>`;
+	formListEl.append(newEl);
+}
+
+function clearInput() {
+	msgEl.value = "";
+	senderEl.value = "";
+	recipientEl.value = "";
+}
+
+function clearFormListEl() {
+	formListEl.innerHTML = "";
+}
